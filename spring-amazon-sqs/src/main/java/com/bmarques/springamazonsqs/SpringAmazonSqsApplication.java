7f7765b84dcb4d4e7f95f4160921ad8426b3473c
Tiago Marques <tiagobm564@gmail.com>
2021-06-11 07:39:22 -0300

    Added Spring project with AWS SQS

---
+++ spring-amazon-sqs/src/main/java/com/bmarques/springamazonsqs/SpringAmazonSqsApplication.java
@@ -0,0 +1,13 @@
+package com.bmarques.springamazonsqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAmazonSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAmazonSqsApplication.class, args);
+	}
+
+}
---
+++ spring-amazon-sqs/src/test/java/com/bmarques/springamazonsqs/SpringAmazonSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmarques.springamazonsqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAmazonSqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
