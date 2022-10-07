87fed009d1e3f8eb57330bea2641311ba180d29a
Tiago Bitencourt Marques <tiagobm564@gmail.com>
2022-10-07 13:52:37 -0300

    Add more config about resilience4j

--- java/spring-resilience4j/spring-resilience4j-api-a/src/main/java/com/bmarques/springresilience4japia/revenue/RevenueService.java
+++ java/spring-resilience4j/spring-resilience4j-api-a/src/main/java/com/bmarques/springresilience4japia/revenue/RevenueService.java
@@ -2,17 +2,21 @@ package com.bmarques.springresilience4japia.revenue;
 
 import com.bmarques.springresilience4japia.invoice.Invoice;
 import com.bmarques.springresilience4japia.invoice.InvoiceClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class RevenueService {
 
     @Autowired
     private InvoiceClient invoiceClient;
 
+    @CircuitBreaker(name = "invoiceApi", fallbackMethod = "fallToGetTotalRevenue")
     public Double getTotalRevenue() {
         List<Invoice> invoices = invoiceClient.getInvoices();
 
@@ -20,5 +24,10 @@ public class RevenueService {
                 .map(Invoice::getValue)
                 .mapToDouble(Double::doubleValue)
                 .sum();
+    }
+
+    public Double fallToGetTotalRevenue(Throwable e) {
+        log.info("Fallback activated");
+        return 0D;
     }
 }
